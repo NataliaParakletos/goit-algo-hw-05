95fab40019e824486ef849055d20cd7a532311d3
NataliaParakletos <153620832+NataliaParakletos@users.noreply.github.com>
2024-02-06 11:28:32 +0200

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,18 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n in cache:
+            return cache[n]
+        if n <= 1:
+            return n
+        else:
+            fib_value = fibonacci(n - 1) + fibonacci(n - 2)
+            cache[n] = fib_value
+            return fib_value
+
+    return fibonacci
+
+fib = caching_fibonacci()
+print(fib(10))
+print(fib(15))
