024e4a72a38f46aee27d5c22f92a319b6a408c76
NataliaParakletos <153620832+NataliaParakletos@users.noreply.github.com>
2024-02-06 13:12:04 +0200

    Add files via upload

---
+++ bot_2.py
@@ -0,0 +1,69 @@
+def input_error(func): 
+    def inner(*args, **kwargs): 
+        try: 
+            return func(*args, **kwargs) 
+        except ValueError: 
+            return "Give me name and phone please." 
+        except KeyError: 
+            return "Contact not found." 
+        except IndexError:
+            return "Enter the argument for the command."
+    return inner 
+
+
+def parse_input(user_input): 
+    cmd, *args = user_input.split() 
+    cmd = cmd.strip().lower() 
+    return cmd, args
+
+@input_error
+def add_contact(args, contacts): 
+    name, phone = args 
+    contacts[name] = phone 
+    return "Contact added." 
+
+@input_error
+def change_contact(args, contacts): 
+    name, new_phone = args 
+    contacts[name] = new_phone 
+    return "Contact updated." 
+
+@input_error
+def show_phone(args, contacts): 
+    name = ''.join(args)
+    return f"Phone number for {name}: {contacts[name]}"
+
+@input_error    
+def show_all(contacts):
+    result = "Contacts list:\n"
+    for name, phone in contacts.items():
+        result += f"{name}: {phone}\n"
+    return result
+
+def main(): 
+    contacts = {} 
+    print("Welcome to the assistant bot!") 
+    while True: 
+        user_input = input("Enter a command: ") 
+        command, args = parse_input(user_input) 
+
+        if command in ["close", "exit"]: 
+            print("Good bye!") 
+            break 
+        elif command == "hello": 
+            print("How can I help you?") 
+        elif command == "add": 
+            print(add_contact(args, contacts)) 
+        elif command == "update": 
+            print(change_contact(args, contacts)) 
+        elif command == "phone" and args: 
+            print(show_phone(args, contacts)) 
+        elif command == "all": 
+            print(show_all(contacts))
+        else: 
+            print("Invalid command.") 
+
+
+if __name__ == "__main__": 
+    main=input_error(main)
+    main()
